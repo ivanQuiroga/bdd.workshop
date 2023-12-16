f12bf0fc6b1fac42a4799b136ab32212fa344fa3
Iván Quiroga <16874249+ivanQuiroga@users.noreply.github.com>
2023-12-16 15:35:35 -0600

    Ajustes interfaz
    Se adaptan los cambios de la interfaz de la calculadora para la aplicación te los test.

--- bdd.workshop.calculator.web/wwwroot/js/site.js
+++ bdd.workshop.calculator.web/wwwroot/js/site.js
@@ -2,3 +2,44 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+function calculate(operator) {
+    document.getElementById('Command').value = operator;
+}
+
+function calculateResult() {
+    let A_TheNumber = parseFloat(document.getElementById('A_TheNumber').value);
+    let B_TheNumber = parseFloat(document.getElementById('B_TheNumber').value);
+    let operator = document.getElementById('Command').value;
+    let result;
+
+    switch (operator) {
+        case '+':
+            result = A_TheNumber + B_TheNumber;
+            break;
+        case '-':
+            result = A_TheNumber - B_TheNumber;
+            break;
+        case '*':
+            result = A_TheNumber * B_TheNumber;
+            break;
+        case '/':
+            result = A_TheNumber / B_TheNumber;
+            break;
+        case '√':
+            result = Math.sqrt(A_TheNumber);
+            break;
+    }
+
+    document.getElementById('result').value = result;
+}
+
+function clearInput() {
+    document.getElementById('Command').value = '';
+}
+
+function clearAll() {
+    document.getElementById('A_TheNumber').value = '';
+    document.getElementById('B_TheNumber').value = '';
+    document.getElementById('Command').value = '';
+    document.getElementById('result').value = '';
+}
